66972c28ae9e726fe974f4c6a289963e88e6f7e1
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-06-13 02:05:04 -0300

    Add button Link

--- src/pages/ShowData.tsx
+++ src/pages/ShowData.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const ShowData = () => {
     return(
+    <>
         <h3>Tela ShowData</h3>
+        <br />
+        <Link to="/" >Voltar para SignUp</Link>
+    </>
     )
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const SignUp = () => {
     return(
+        <>
         <h3>Tela SignUp</h3>
+        <br />
+        <Link to="/exibir" >Ir para page ShowData</Link>
+        </>
     )
 }
